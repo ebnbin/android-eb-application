a2377b1679a7d5e485d06c2f2310b4ff58560a81
Ebn Zhang <ebnbin@gmail.com>
2017-03-21 22:50:00 +0800

    Initial commit

---
+++ sample/src/main/java/com/ebnbin/ebapplication/sample/EBApplicationSampleApplication.java
@@ -0,0 +1,6 @@
+package com.ebnbin.ebapplication.sample;
+
+import com.ebnbin.eb.base.EBApplication;
+
+public final class EBApplicationSampleApplication extends EBApplication {
+}
---
+++ sample/src/main/java/com/ebnbin/ebapplication/sample/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ebnbin.ebapplication.sample;
+
+import com.ebnbin.eb.base.EBActivity;
+
+public final class MainActivity extends EBActivity {
+}
