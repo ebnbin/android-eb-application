30199aab5db465245ac66323bc70caf08e3251cb
Ebn Zhang <ebnbin@gmail.com>
2017-03-27 13:28:37 +0800

    Add methods about onCreateView in EBFragment

--- eb-application/src/main/java/com/ebnbin/ebapplication/base/EBFragment.java
+++ eb-application/src/main/java/com/ebnbin/ebapplication/base/EBFragment.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.ebnbin.ebapplication.net.NetCallback;
 import com.ebnbin.ebapplication.net.NetHelper;
@@ -86,6 +90,65 @@ public abstract class EBFragment extends Fragment {
      *         If {@link #getArguments()} returns {@code null}, an empty {@link Bundle} will be used.
      */
     protected void onInitArguments(@NonNull Bundle args) {
+    }
+
+    //*****************************************************************************************************************
+    // OnCreateView.
+
+    /**
+     * This method is overrode, a new way to initialize content view is to override {@link #overrideContentView()} and
+     * returns the content view, or to override {@link #overrideContentViewRes()} and returns the resource id of
+     * content view. If both of these two methods returns valid value, the return of {@link #overrideContentView()}
+     * will be used, and the return of {@link #overrideContentViewRes()} will be ignored. After that, override
+     * {@link #onInitContentView(View)} to initialize views with the given content view. If content view if
+     * {@code null}, method {@link #onInitContentView(View)} will not be called. <br>
+     * If this method is overrode in subclasses, this way of initializing content view will be ignored, and methods
+     * {@link #overrideContentView()},{@link #overrideContentViewRes()} and {@link #onInitContentView(View)} will not
+     * be called.
+     *
+     * @see #overrideContentView()
+     * @see #overrideContentViewRes()
+     * @see #onInitContentView(View)
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View contentView = overrideContentView();
+        if (contentView == null) {
+            int contentViewRes = overrideContentViewRes();
+            if (contentViewRes == 0) {
+                return null;
+            }
+
+            contentView = inflater.inflate(contentViewRes, container, false);
+        }
+
+        onInitContentView(contentView);
+
+        return contentView;
+    }
+
+    /**
+     * @see #onCreateView(LayoutInflater, ViewGroup, Bundle)
+     */
+    @Nullable
+    protected View overrideContentView() {
+        return null;
+    }
+
+    /**
+     * @see #onCreateView(LayoutInflater, ViewGroup, Bundle)
+     */
+    @LayoutRes
+    protected int overrideContentViewRes() {
+        return 0;
+    }
+
+    /**
+     * @see #onCreateView(LayoutInflater, ViewGroup, Bundle)
+     */
+    protected void onInitContentView(@NonNull View contentView) {
     }
 
     //*****************************************************************************************************************
