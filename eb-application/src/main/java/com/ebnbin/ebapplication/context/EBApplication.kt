3abffcb937eed4bc3c9dc85f4275be021dc12df8
Ebn Zhang <ebnbin@gmail.com>
2017-06-21 00:04:43 +0800

    Add abstract function 'applicationId' in EBApplication

--- eb-application/src/main/java/com/ebnbin/ebapplication/context/EBApplication.kt
+++ eb-application/src/main/java/com/ebnbin/ebapplication/context/EBApplication.kt
@@ -1,7 +1,7 @@
 package com.ebnbin.ebapplication.context
 
 import android.app.Application
-
+import com.ebnbin.eb.util.EBUtil
 import com.ebnbin.ebapplication.net.NetHelper
 
 /**
@@ -11,8 +11,18 @@ abstract class EBApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        initEBUtil()
         initNetHelper()
     }
+
+    private fun initEBUtil() {
+        EBUtil.init(applicationId())
+    }
+
+    /**
+     * Returns application's `BuildConfig.APPLICATION_ID`.
+     */
+    protected abstract fun applicationId(): String
 
     private fun initNetHelper() {
         NetHelper.init()
--- sample/src/main/java/com/ebnbin/ebapplication/sample/SampleApplication.kt
+++ sample/src/main/java/com/ebnbin/ebapplication/sample/SampleApplication.kt
@@ -1,16 +1,7 @@
 package com.ebnbin.ebapplication.sample
 
-import com.ebnbin.eb.util.EBUtil
 import com.ebnbin.ebapplication.context.EBApplication
 
 class SampleApplication : EBApplication() {
-    override fun onCreate() {
-        super.onCreate()
-
-        initEBUtil()
-    }
-
-    private fun initEBUtil() {
-        EBUtil.init(BuildConfig.APPLICATION_ID)
-    }
+    override fun applicationId() = BuildConfig.APPLICATION_ID
 }
