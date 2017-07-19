a15ffde43a99a7853757e270513d5498a73e3dd9
Ebn Zhang <ebnbin@gmail.com>
2017-07-19 11:00:31 +0800

    Update Kotlin version to 1.1.3-2; Delete module 'sample'

--- sample/src/main/java/com/ebnbin/ebapplication/sample/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ebnbin.ebapplication.sample
-
-import com.ebnbin.ebapplication.context.EBActivity
-
-class MainActivity : EBActivity()
--- sample/src/main/java/com/ebnbin/ebapplication/sample/SampleApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.ebnbin.ebapplication.sample
-
-import com.ebnbin.ebapplication.context.EBApplication
-
-class SampleApplication : EBApplication() {
-    override fun applicationId() = BuildConfig.APPLICATION_ID
-}
